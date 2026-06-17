ef70dbb474a03a16c84d4e7e81a6d319235a80eb
huitema <huitema@huitema.net>
2026-06-17 10:59:00 -0700

    report and updates v1

---
+++ scripts/test_report.py
@@ -0,0 +1,113 @@
+# Write a test report from the list of results.
+#
+# The statistics directory (./tmp/stats) has one CSV file per test case. The CSV
+# has column for:
+#  - result: Success or Fail
+#  - rep: from 1 to N, the repeat number
+#  - time: the execution time of the test
+#  - av_latency: for "media" time of tests, the average frame latency.
+#  - max_latency: for "media" time of tests, the max frame latency.
+#  - test: the name of the test
+#
+# we could do graphs, or compute key statistics like media, top 95% and max for
+# the time, av_latency and max_latency. We can do that with panda, e.g.:
+#   q75 = df['Column2'].quantile(q=0.75)
+#   qmax = df['Column2'].max()
+#
+# The name of the file is normally the name of the test. That name is
+# composed the for: <alg_name> "_" <test_case> ".csv". 
+#
+# We should group the test by test cases, and possibly the test cases by related
+# groups like "single connection", "wi-fi", "Compete", etc. We may see results
+# for c4, bbr and cubic. We want to present them in a comparison table.
+# Maybe write all that in markdown.
+#
+
+
+
+import sys
+import os
+from pathlib import Path
+import pandas as pd
+import traceback
+
+# dict of valid algorithms
+algo_dict = {
+    "c4":0,
+    "bbr":1,
+    "cubic":2 }
+
+# Operations on a test report.
+class test_report:
+    def __init__(self, test_case, algo):
+        self.df = None
+        self.test_case = test_case
+        self.algo = algo
+
+
+    def load(self, file_name):
+        self.df = pd.read_csv(file_name)
+    
+    def average(self, metric):
+        x = self.df[metric].mean()
+        return x
+
+class test_case_group:
+    def __init__(self, tc):
+        self.tc = tc
+        self.alg_report = [ None, None, None ]
+
+
+    def do_report(self, F):
+        for metric in [ 'time', 'av_latency', 'max_latency' ]:
+                sm = "| " + self.tc + " | " + metric + " |"
+                has_metric = False
+                for i in range(0,3):
+                    if self.alg_report[i] == None:
+                        sm += " |"
+                    else:
+                        x = self.alg_report[i].average(metric)
+                        sm += " " + str(x) + " |"
+                        if x > 0:
+                            has_metric = True
+                if has_metric:
+                    F.write(sm + '\n')
+
+# Grouping of reports
+
+class report_list:
+    def __init__(self):
+        self.test_cases=dict()
+
+    def add_dir(self, dir_path):
+        for report_name in os.listdir(dir_path):
+            if report_name.endswith(".csv"):
+                algo_case = report_name[:-4]
+                algo_case_part = algo_case.split("_")
+                if len(algo_case_part) > 1:
+                    algo = algo_case_part[0]
+                    if algo in algo_dict:
+                        tc = algo_case[(len(algo) + 1):]
+                        rp = test_report(algo, tc)
+                        rp.load(os.path.join(dir_path, report_name))
+                        if not tc in self.test_cases:
+                            self.test_cases[tc] = test_case_group(tc)
+                        self.test_cases[tc].alg_report[algo_dict[algo]] = rp
+
+    def do_report(self, F):
+        F.write("# Statistics\n")
+        F.write("Here is a collection of statistics on all test cases.\n\n")
+        F.write("| test case | metric | c4 | bbr | cubic |\n")
+        F.write("|------|-----| ---:| ---:| ---:|\n")
+        for tc in self.test_cases:
+            self.test_cases[tc].do_report(F)
+
+
+# main
+
+rl = report_list()
+
+rl.add_dir(sys.argv[1])
+
+with open(sys.argv[2], "wt") as F:
+    rl.do_report(F)
